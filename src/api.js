29fda7f6391e4e12764c2aa4824fed28ac2ba535
unknown <alex3953898@yandex.ru>
2021-04-24 21:21:09 +0300

    task-5: refactor api

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+const apiUrl = 'http://localhost:4000';
+
 async function getMovies(searchBy, sortBy, category) {
   const searchParams = new URLSearchParams({
     search: searchBy,
@@ -6,19 +8,19 @@ async function getMovies(searchBy, sortBy, category) {
     filter: category == 'All' ? '' : category,
     sortOrder: sortBy == 'title' ? 'asc' : 'desc'
   });
-  const moviesResponse = await fetch(`http://localhost:4000/movies?${searchParams}`);
+  const moviesResponse = await fetch(`${apiUrl}/movies?${searchParams}`);
   const movies = await moviesResponse.json();
   return movies.data;
 }
 
 async function getMovieDetails(id) {
-  const movieDetailsResponse = await fetch(`http://localhost:4000/movies/${id}`);
+  const movieDetailsResponse = await fetch(`${apiUrl}/movies/${id}`);
   const movieDetails = await movieDetailsResponse.json();
   return movieDetails;
 }
 
 async function updateMovieDetails(movieDetails) {
-  const movieDetailsResponse = await fetch(`http://localhost:4000/movies`, {
+  const movieDetailsResponse = await fetch(`${apiUrl}/movies`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
